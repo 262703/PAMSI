9f17bfad75206e04fe8b58cceea475cbb092e119
262703 <77827417+262703@users.noreply.github.com>
2022-05-26 17:12:39 +0200

    Delete Proj 2 directory

--- Proj 2/inc/BF.hpp
+++
@@ -1,9 +0,0 @@
-#include "list.hpp"
-#include "matrix.hpp"
-
-#include <chrono>
-#include <iomanip>
-
-void MatrixBellmanFord(Matrix GraphMatrix, std::fstream& File);
-void ListBellmanFord(List GraphList, std::fstream& File);
-void OnMyWay(int* PayToGo, int Edg, int* Way, std::fstream& File);
